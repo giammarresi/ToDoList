956a8f2ea6cc7eaa8d4b5122654d02f78db4f53a
Yves <yves.giammarresi@edge5.com>
2014-10-02 14:16:17 +0200

    Initial Commit

---
+++ ToDoList/GIAMMIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GIAMMIAppDelegate.h
+//  ToDoList
+//
+//  Created by Yves on 02.10.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GIAMMIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Yves on 02.10.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GIAMMIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GIAMMIAppDelegate class]));
+    }
+}
